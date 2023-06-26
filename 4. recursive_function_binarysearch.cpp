52850a6b93b79d9572ebde0433dc3fb7c93ecf59
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-06-26 11:30:07 +0530

    Add files via upload

---
+++ 4. recursive_function_binarysearch.cpp
@@ -0,0 +1,33 @@
+//Recursive Program: 
+#include<stdio.h> 
+int binarysearch(int *arr,int e,int l,int r) 
+{ 
+ int m; 
+ if(l>r) 
+  return -1; 
+ else 
+{ 
+  m=(l+r)/2; 
+  if(arr[m]==e) 
+   return m; 
+  if(arr[m]<e) 
+   l=m+1; 
+  if(arr[m]>e) 
+   r=m-1; 
+ } 
+ return binarysearch(arr,e,l,r); 
+} 
+int main() 
+{ 
+ int n,e,i,j,k,t; 
+ scanf("%d",&n); 
+ int l=0,r=n-1,arr[n]; 
+ for(i=0;i<n;i++) 
+  scanf("%d",&arr[i]); 
+ scanf("%d",&e); 
+ k= binarysearch(arr,e,l,r); 
+ if(k!=-1) 
+  printf("Element found at index %d",k); 
+ else 
+  printf("Element not found"); 
+}
